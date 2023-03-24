bcc326fe836348ce3e1ab748f0c20bf4b69a9a3d
Jannik <jannikcsanidad@gmail.com>
2023-03-24 23:59:07 +0800

    changed webhook to discord bot

--- bh_notification_termux.py
+++ bh_notification_termux.py
@@ -5,15 +5,11 @@ import datetime
 import requests
 import json
 from PIL import Image, ImageOps, ImageFilter
-import logging
-
-debug_run_count = 0
+import discord
+import asyncio
 
 # Set the path to the Tesseract executable
 pytesseract.pytesseract.tesseract_cmd = "/data/data/com.termux/files/usr/bin/tesseract"
-
-# Set up logging
-logging.basicConfig(filename='http_request_errors.log', level=logging.DEBUG)
 
 # Set the banner text to monitor for
 banner_texts = [    
@@ -50,15 +46,20 @@ banner_texts = [
     "Naght Sieger will",
     "Coelacanth will",]
 
-
-# Set the Discord webhook URL
-discord_webhook_url = "https://discord.com/api/webhooks/1088817998221033562/pWBhsvdHBPvcK00JwUmdlMzAX1xCXMPRU1cQ8Vn6IPszBHXtWlJXdsFAo6ZROQNDg9oH"
-
 # Set the interval in seconds to check the game screen
 check_interval = 3
 
 # Keep track of the last time the banner text was detected
 last_detection_time = datetime.datetime.now() - datetime.timedelta(seconds=check_interval)
+
+# Set the Discord bot token
+bot_token = "MTA4ODg1MTE1NjA4NTc4NDU4Nw.G5Hpkp.BZuja-VwIvK3Ouox-nOKgRjCQi3BM9QhKi_KPg"
+
+# Set the ID of the Discord channel to send messages to
+channel_id = "1087725231159914606"
+
+# Create a Discord client
+client = discord.Client()
 
 # Pre-Process Image for OCR
 def preprocess_image(image):
@@ -77,135 +78,124 @@ def preprocess_image(image):
 
     return image
 
+# Event listener for when the bot is ready
+@client.event
+async def on_ready():
 
-# Loop indefinitely
-while True:
-    # Get the current time
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    # Loop indefinitely
+    while True:
+        # Get the current time
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-    # Capture a screenshot of the device screen using the screencap command
-    os.system('screencap /storage/emulated/0/screenshot.png')
+        # Capture a screenshot of the device screen using the screencap command
+        os.system('screencap /storage/emulated/0/screenshot.png')
 
-    # Open the captured screenshot using the PIL module
-    with Image.open('/storage/emulated/0/screenshot.png') as screenshot:
+        # Open the captured screenshot using the PIL module
+        with Image.open('/storage/emulated/0/screenshot.png') as screenshot:
 
-        # Pre-Process the Image Object
-        # image = screenshot.resize((screenshot.width * 3, screenshot.height * 3))
-        image = preprocess_image(screenshot)
+            # Pre-Process the Image Object
+            # image = screenshot.resize((screenshot.width * 3, screenshot.height * 3))
+            image = preprocess_image(screenshot)
 
-        image.save('/storage/emulated/0/screenshot_processed.png')
+            image.save('/storage/emulated/0/screenshot_processed.png')
 
 
-        # Extract text from the image using Tesseract 
-        try:
-            extracted_text = pytesseract.image_to_string(image, config="--psm 6")
-            print(extracted_text)
-        except pytesseract.TesseractError as e:
-            print(f"Error: {e}")
-            extracted_text = ""
+            # Extract text from the image using Tesseract 
+            try:
+                extracted_text = pytesseract.image_to_string(image, config="--psm 6")
+                print(extracted_text)
+            except pytesseract.TesseractError as e:
+                print(f"Error: {e}")
+                extracted_text = ""
 
-        # Check if the banner text is present in the extracted text
-        for banner_text in banner_texts:
-            if banner_text in extracted_text:
-                # Calculate the time since the last detection
-                time_since_last_detection = datetime.datetime.now() - last_detection_time
-                debug_run_count += 1
-                logging.info(f"Debug run: {debug_run_count} First Checkpoint")
+            # Check if the banner text is present in the extracted text
+            for banner_text in banner_texts:
+                if banner_text in extracted_text:
+                    # Calculate the time since the last detection
+                    time_since_last_detection = datetime.datetime.now() - last_detection_time
 
-                # Only send a notification if enough time has elapsed since the last detection
-                if time_since_last_detection > datetime.timedelta(seconds=10):
-                    # Update the last detection time
-                    last_detection_time = datetime.datetime.now()
+                    # Only send a notification if enough time has elapsed since the last detection
+                    if time_since_last_detection > datetime.timedelta(seconds=10):
+                        # Update the last detection time
+                        last_detection_time = datetime.datetime.now()
+        
+                        # Choose a message to send based on the detected banner text
+                        if banner_text == "Mistress will":
+                            message_text = "Mistress will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Phreeoni will":
+                            message_text = "Phreeoni will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Kraken will":
+                            message_text = "Kraken will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Eddga will":
+                            message_text = "Eddga will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Maya will":
+                            message_text = "Maya will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Orc Hero will":
+                            message_text = "Orc Hero will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Pharaoh will":
+                            message_text = "Pharaoh will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Orc Lord will":
+                            message_text = "Orc Lord will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Amon Ra will":
+                            message_text = "Amon Ra will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Doppelganger will":
+                            message_text = "Doppelganger will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Overseer of Time":
+                            message_text = "Overseer of Time will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Morroc will":
+                            message_text = "Morroc will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Lost Dragon will":
+                            message_text = "Lost Dragon will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Tao Gunka will":
+                            message_text = "Tao Gunka will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Fallen Bishop will":
+                            message_text = "Fallen Bishop will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Lord of the Dead will":
+                            message_text = "Lord of the Dead will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Eclipse will":
+                            message_text = "Eclipse will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Dragon Fly will":
+                            message_text = "Dragon Fly will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Mastering will":
+                            message_text = "Mastering will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Ghostring will":
+                            message_text = "Ghostring will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Toad will":
+                            message_text = "Toad will be spawning soon! Warriors, charge!"
+                        elif banner_text == "King Dramoh will":
+                            message_text = "King Dramoh will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Deviling will":
+                            message_text = "Deviling will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Angeling will":
+                            message_text = "Angeling will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Dark Priest will":
+                            message_text = "Dark Priest will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Vagabond Wolf will":
+                            message_text = "Vagabond Wolf will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Chimera will":
+                            message_text = "Chimera will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Mysteltainn will":
+                            message_text = "Mysteltainn will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Ogretooth will":
+                            message_text = "Ogretooth will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Necromancer will":
+                            message_text = "Necromancer will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Naght Sieger will":
+                            message_text = "Naght Sieger will be spawning soon! Warriors, charge!"
+                        elif banner_text == "Coelacanth will":
+                            message_text = "Coelacanth will be spawning soon! Warriors, charge!"
 
-                    logging.info(f"Debug run: {debug_run_count} Second Checkpoint")
-    
-                    # Choose a message to send based on the detected banner text
-                    if banner_text == "Mistress will":
-                        message_text = "Mistress will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Phreeoni will":
-                        message_text = "Phreeoni will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Kraken will":
-                        message_text = "Kraken will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Eddga will":
-                        message_text = "Eddga will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Maya will":
-                        message_text = "Maya will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Orc Hero will":
-                        message_text = "Orc Hero will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Pharaoh will":
-                        message_text = "Pharaoh will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Orc Lord will":
-                        message_text = "Orc Lord will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Amon Ra will":
-                        message_text = "Amon Ra will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Doppelganger will":
-                        message_text = "Doppelganger will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Overseer of Time":
-                        message_text = "Overseer of Time will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Morroc will":
-                        message_text = "Morroc will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Lost Dragon will":
-                        message_text = "Lost Dragon will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Tao Gunka will":
-                        message_text = "Tao Gunka will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Fallen Bishop will":
-                        message_text = "Fallen Bishop will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Lord of the Dead will":
-                        message_text = "Lord of the Dead will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Eclipse will":
-                        message_text = "Eclipse will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Dragon Fly will":
-                        message_text = "Dragon Fly will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Mastering will":
-                        message_text = "Mastering will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Ghostring will":
-                        message_text = "Ghostring will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Toad will":
-                        message_text = "Toad will be spawning soon! Warriors, charge!"
-                    elif banner_text == "King Dramoh will":
-                        message_text = "King Dramoh will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Deviling will":
-                        message_text = "Deviling will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Angeling will":
-                        message_text = "Angeling will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Dark Priest will":
-                        message_text = "Dark Priest will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Vagabond Wolf will":
-                        message_text = "Vagabond Wolf will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Chimera will":
-                        message_text = "Chimera will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Mysteltainn will":
-                        message_text = "Mysteltainn will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Ogretooth will":
-                        message_text = "Ogretooth will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Necromancer will":
-                        message_text = "Necromancer will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Naght Sieger will":
-                        message_text = "Naght Sieger will be spawning soon! Warriors, charge!"
-                    elif banner_text == "Coelacanth will":
-                        message_text = "Coelacanth will be spawning soon! Warriors, charge!"
+                        # Find the channel to send messages to
+                        channel = client.get_channel(channel_id)
 
-                    # Create the Discord notification message
-                    message = {
-                        "content": f"{message_text}"
-                    }
+                        # Send the message to the channel
+                        await channel.send(message_text)
 
-                    # Send the notification to the Discord webhook
-                    response = requests.post(discord_webhook_url, data=json.dumps(message), headers={"Content-Type": "application/json"})
+                        # Break out of the loop once a banner text is detected and a message is sent
+                        break
 
-                    logging.info(f"Debug run: {debug_run_count} After Request:")
-                    logging.info(f"{response} \n Status Code: {response.status_code}")
+        # Wait for the specified interval before checking again
+        await asyncio.sleep(check_interval)
 
-                    # Check if the notification was sent successfully
-                    if response.status_code == 204:
-                        print(f"Notification sent at {current_time}")
-
-                    # Check for errors in the response
-                    if response.status_code != 200:
-                        logging.error(f'Debug run: {debug_run_count} HTTP request failed with status code {response.status_code} for webhook URL: {discord_webhook_url}')
-
-                    # Break out of the loop once a banner text is detected and a message is sent
-                    break
-
-    # Wait for the specified interval before checking again
-    time.sleep(check_interval)
+# Start the bot
+client.run(bot_token)
